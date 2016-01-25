05bf76d54dca55e3fccc0ec2b78e8a14aefc6e06
jdmor <jona.morrissey@gmail.com>
2016-01-25 19:55:49 +0000

    Song submissions added to database

--- app/actions.rb
+++ app/actions.rb
@@ -1,7 +1,18 @@
 get '/' do
   @songs = Song.all
-  
+
   erb :index
+end
+
+post '/' do
+  @song = Song.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  @song.save!
+
+  halt 201
 end
 
 get '/new' do
