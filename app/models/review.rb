4abdb39fa88727175f7f375cb68241f52a071cdf
jdmor <jona.morrissey@gmail.com>
2016-01-26 21:40:49 +0000

    Review validations - user can review each song once max

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :song
   belongs_to :user
+
+  validates :song_id, presence: true, uniqueness: { scope: :user_id,
+    message: 'can only be reviewed once per user'}
+  validates :user_id, presence: true
+  validates :content, presence: true
 end
