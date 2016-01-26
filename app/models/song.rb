a0a1c1c21f7d534a57ce64b8939fa0c7c201af01
jdmor <jona.morrissey@gmail.com>
2016-01-26 07:17:42 +0000

    Associate user and song models

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,7 @@
 class Song < ActiveRecord::Base
+
+  belongs_to :user
+
   validates :title, presence: true
   validates :author, presence: true
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_many :songs
+
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 end
