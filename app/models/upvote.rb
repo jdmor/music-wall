23e9e50ecb1d37e70fa53c37b2c15886af8614ac
jdmor <jona.morrissey@gmail.com>
2016-01-26 07:52:27 +0000

    Upvote validation

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,4 +1,12 @@
 class Upvote < ActiveRecord::Base
   belongs_to :user
   belongs_to :song
+
+  validates :song_id, presence: true
+  validates :user_id, presence: true
+  validate :user_can_upvote
+
+  def first_upvote
+    errors.add(:user_can_upvote, 'a song only once.') if Upvote.find_by song_id: song_id, user_id: user_id
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126070846) do
+ActiveRecord::Schema.define(version: 20160126071827) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title",      null: false
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20160126070846) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "upvotes", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "song_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
