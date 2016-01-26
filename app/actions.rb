4953a459f51ee989eb7820b385fa71c07a46dfe1
jdmor <jona.morrissey@gmail.com>
2016-01-26 02:11:25 +0000

    Account page

--- app/actions.rb
+++ app/actions.rb
@@ -4,6 +4,18 @@ get '/' do
   erb :index
 end
 
+get '/new' do
+  erb :'new/index'
+end
+
+get '/login' do
+  erb :'login/index'
+end
+
+get '/create-account' do
+  erb :'create-account/index'
+end
+
 post '/' do
   @song = Song.new(
     title: params[:title],
@@ -18,19 +30,24 @@ post '/' do
   end
 end
 
-get '/new' do
-  erb :'new/index'
-end
-
-get '/login' do
-  erb :'login/index'
-end
-
 post '/login' do
   if params[:email].empty? || params[:password].empty?
     erb :'login/index'
   else
     user = User.find_by(email: params[:email], password: params[:password])
     user ? redirect('/') : erb(:'login/index')
+  end
+end
+
+post '/create-account' do
+  @user = User.new(
+    email: params[:email],
+    password: params[:password]
+  )
+
+  if @user.save
+    redirect '/'
+  else
+    erb :'create-account/index'
   end
 end
