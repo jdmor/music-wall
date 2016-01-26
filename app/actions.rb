e19429e64dfed3754dea5606f58646f2dff2f36e
jdmor <jona.morrissey@gmail.com>
2016-01-26 22:03:18 +0000

    Link songs on home to song details page

--- app/actions.rb
+++ app/actions.rb
@@ -12,12 +12,21 @@ helpers do
     Upvote.find_by song_id: song_id, user: user_id
   end
 
+  def allowed_to_review?(song_id)
+    true unless Review.find_by song_id: song_id, user: current_user
+  end
+
 end
 
 get '/' do
-  @songs = Song.select('songs.id, songs.title, songs.author, songs.url, songs.user_id, COUNT(upvotes.song_id) AS upvote_count')
-  @songs = @songs.joins('LEFT OUTER JOIN upvotes ON upvotes.song_id = songs.id')
-  @songs = @songs.group('songs.id').order('upvote_count DESC')
+  # Original way
+  # @songs = Song.select('songs.id, songs.title, songs.author, songs.url, songs.user_id, COUNT(upvotes.song_id) AS upvote_count')
+  # @songs = @songs.joins('LEFT OUTER JOIN upvotes ON upvotes.song_id = songs.id')
+  # @songs = @songs.group('songs.id').order('upvote_count DESC')
+
+  # New and improved - `includes` will do a LEFT OUTER JOIN
+  # an `references` allows you to write SQL that references the included table.
+  @songs = Song.includes(:upvotes).group(:id).order('COUNT(upvotes.id) DESC').references(:upvotes)
   erb :index
 end
 
@@ -35,7 +44,7 @@ end
 
 get '/details/:id' do
   @song = Song.find_by id: params[:id]
-
+  @reviews = Review.where(song_id: params[:id]).order(created_at: :desc)
   if @song
     erb :'details/index'
   else
@@ -96,4 +105,17 @@ post '/upvote' do
   else
     redirect '/login' 
   end
+end
+
+post '/details/:id' do
+  # saving reviews to the page
+  # removing reviews from the page
+  
+  review = Review.new song_id: params[:id], user_id: session['user_id'], content: params[:content]
+  if review.save
+    @review_saved = true
+  else
+    @review_saved = false
+  end
+  redirect "/details/#{params[:id]}"
 end
