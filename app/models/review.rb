f876cabee57188edb4895881a07172580d2c37b9
jdmor <jona.morrissey@gmail.com>
2016-01-27 03:10:38 +0000

    Validate rating for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,4 +6,5 @@ class Review < ActiveRecord::Base
     message: 'can only be reviewed once per user'}
   validates :user_id, presence: true
   validates :content, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
 end
